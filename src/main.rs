2bd022c625303261c7c8303e5932ce8d5069e478
cuds <ana@r.c>
2025-02-23 23:25:16 +0100

    main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let mut cpu = CPU::new();
+    let memory = vec![0; 4 * 1024 * 1024]; // Simulando 4MB de RAM
+    
+    let instruction = cpu.fetch(&memory);
+    cpu.decode_execute(instruction);
 }
+
