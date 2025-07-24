0f8f6395cd3b6ff5d040a17b6daa0b7adb37e769
Csatinapppple <cstinagarplz@hotmail.com>
2025-07-24 10:44:29 +0200

    ad

--- src/processor.rs
+++ src/processor.rs
@@ -1,5 +1,5 @@
 
-let BIOS_START = 0xbfc00000;
+const BIOS_START = 0xbfc00000;
 
 
 struct CPU {
@@ -19,37 +19,9 @@ impl CPU {
         }
     }
     
-    fn fetch(&self, memory: &[u8]) -> u32 {
-        // Busca uma instrução da memória (4 bytes por instrução MIPS)
-        let addr = self.pc as usize;
-        u32::from_be_bytes([memory[addr], memory[addr + 1], memory[addr + 2], memory[addr + 3]])
-    }
     
-    fn decode_execute(&mut self, instruction: u32) {
-        let opcode = (instruction >> 26) & 0x3F;
-        match opcode {
-            0x00 => self.execute_r_type(instruction), // Instruções R-type
-            _ => println!("Opcode {:X} não implementado", opcode),
-        }
-    }
-    
-    fn execute_r_type(&mut self, instruction: u32) {
-        let funct = instruction & 0x3F;
-        match funct {
-            0x20 => println!("ADD (soma) implementado futuramente"),
-            _ => println!("Função {:X} não implementada", funct),
-        }
-    }
-
-    
-    fn cycle(&mut self, memory: &[u8]){
-        
-        let instruction = fetch(memory);
-
-        decode_execute(instruction);
-
-        self.pc += self.pc.wrapping_add(4);
-        
+    fn decode(opcode: u32){
+           
     }
 
 }
